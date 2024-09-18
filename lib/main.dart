08aadf60e1d6fd3c723a6a7871f0442b52cd3892
Mateusz Okrój <mateuszokroj1@gmail.com>
2024-09-19 00:04:42 +0200

    Layout prepared. Created backend methods.

--- lib/main.dart
+++ lib/main.dart
@@ -11,42 +11,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Przelicznik akustyczny',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Przelicznik akustyczny'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,17 +31,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  double _distance = 100;
+  double _phase = 360;
+  double _freq = 1;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void setDist(double value) {
+
+  }
+
+  void setPhase(double value) {
+
+  }
+
+  void setFreq(double value) {
+
   }
 
   @override
@@ -77,49 +56,62 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Column(
+          children: [
+            Container(
+                margin: const EdgeInsets.all(30),
+                child: const Text(
+                    'Formularz pozwalający na przeliczenie odległości przechodzenia fali akustycznej w powietrzu przy temperaturze 20°C.\n')),
+            Container(
+                margin: const EdgeInsets.all(30),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: 'Odległość', suffixText: 'cm'),
+                  initialValue: '100.0',
+                  validator: (String? value) {
+                    if (value == null) {
+                      return null;
+                    }
+
+                    var reg = RegExp('^\\d+([\\.\\,]\\d+)?\$');
+                    return reg.hasMatch(value) ? value : null;
+                  },
+                )),
+            Container(
+                margin: const EdgeInsets.all(30),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: 'Kąt przesunięcia fazy', suffixText: '°'),
+                  initialValue: '360.0',
+                  validator: (String? value) {
+                    if (value == null) {
+                      return null;
+                    }
+
+                    var reg = RegExp('^\\d+([\\.\\,]\\d+)?\$');
+                    return reg.hasMatch(value) ? value : null;
+                  },
+                )),
+            Container(
+                margin: const EdgeInsets.all(30),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: 'Częstotliwość', suffixText: 'Hz'),
+                  initialValue: '1',
+                  validator: (String? value) {
+                    if (value == null) {
+                      return null;
+                    }
+
+                    var reg = RegExp('^\\d+([\\.\\,]\\d+)?\$');
+                    return reg.hasMatch(value) ? value : null;
+                  },
+                )),
+          ],
+        ));
   }
 }
